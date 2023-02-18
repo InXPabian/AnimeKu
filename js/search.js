d8a7e9372d6d531047ed7088bedaac388be26ab7
InXPabian <99057280+InXPabian@users.noreply.github.com>
2023-02-18 20:35:12 +0700

    Add files via upload

---
+++ js/search.js
@@ -0,0 +1,394 @@
+const anime = [
+    {
+        id: '1',
+        titleName: 'Akiba Maido Sensou',
+        poster: "images/AkibaMaid.jpg",
+        link: 'akibamaid.html'
+    }, 
+    {
+        id: '2',
+        titleName: 'Yuusha Party wo Tsuihou sareta Beast Tamer, Saikyoushu no Nekomimi Shoujo to Deau',
+        poster: "images/beasttamer.jpeg",
+        link: 'beasttamer.html'
+    },
+    {
+        id: '3',
+        titleName: 'Fuufu Ijou, Koibito Miman',
+        poster: "images/fufuu.jpeg",
+        link:'fufuuijou.html'
+    },
+    {
+        id: '4',
+        titleName: 'Chainsaw Man',
+        poster: "images/chainsawman.jpeg",
+        link: 'chainsaw.html'
+    },
+    {
+        id: '5',
+        titleName: 'Renai Flops',
+        poster: "images/RenaiFlops.jpg",
+        link: 'renaiflops.html' 
+    },
+    {
+        id: '6',
+        titleName: 'Mairimashita! Iruma-kun 3rd Season',
+        poster: "images/irumas3.jpeg",
+        link: 'mairimasita.html'
+    },
+    {
+        id: '7',
+        titleName: 'Tensei shitara Ken Deshita',
+        poster: "images/pedangrei.jpeg",
+        link: 'pedangrei.html'
+    },
+    {
+        id: '8',
+        titleName: 'Kage no Jitsuryokusha ni Naritakute!',
+        poster: "images/cid.jpeg",
+        link: 'cid.html'
+    },
+    {
+        id: '9',
+        titleName: 'SPY x FAMILY Part 2',
+        poster: "images/spypt2.jpeg",
+        link: 'spypt2.html'
+    },
+    {
+        id: '10',
+        titleName: 'Uzaki-chan wa Asobitai! Double',
+        poster: "images/uzakis2.jpeg",
+        link: 'uzakis2.html'
+    },
+    {
+        id: '11',
+        titleName: 'Youkoso Jitsuryoku Shijou Shugi no Kyoushitsu e',
+        poster: "images/classroom.jpg",
+        link: 'classroom.html'
+    },
+    {
+        id: '12',
+        titleName: 'Sono Bisque Doll wa Koi wo Suru',
+        poster: "images/mydressup.jpg",
+        link: 'mydress.html'
+    },
+    {
+        id: '13',
+        titleName: 'Tantei wa Mou, Shindeiru',
+        poster: "images/detectiv.jpg",
+        link: 'tateimo.html'
+    },
+    {
+        id: '14',
+        titleName: 'Date A Live',
+        poster: "https://cdn.myanimelist.net/images/anime/13/44844.jpg",
+        link: 'datealive.html'
+    },
+    {
+        id: '15',
+        titleName: 'Spy x Family',
+        poster: "https://cdn.myanimelist.net/images/anime/1441/122795.jpg",
+        link: 'spyxfam.html'
+    },
+    {
+        id: '16',
+        titleName: 'Nisekoi: False Love',
+        poster: "images/Nisekoi.jpg",
+        link: 'nisekoi.html'
+    },
+    {
+        id: '17',
+        titleName: 'Kono Subarashii Sekai ni Shukufuku wo!',
+        poster: "images/konosuba.jpg",
+        link: 'konosuba.html'
+    },
+    {
+        id: '18',
+        titleName: 'Tokyo Revengers',
+        poster: "images/revenge.jpg",
+        link: 'tokyorev.html'
+    },
+    {
+        id: '19',
+        titleName: 'Tanaka-kun wa Itsumo Kedaruge',
+        poster: "images/tanakakun.jpg",
+        link: 'tanaka.html'
+    },
+    {
+        id: '20',
+        titleName: 'Tsuujou Kougeki ga Zentai Kougeki de Ni-kai Kougeki no Okaasan wa Suki Desu ka?',
+        poster: "images/mamakun.jpg",
+        link: 'mamakun.html'
+    },
+    {
+        id: '21',
+        titleName: 'Kyokou Suiri Season 2',
+        poster: "https://cdn.myanimelist.net/images/anime/1901/131653.jpg",
+        link: 'kyoko2.html'
+    },
+    {
+        id: '22',
+        titleName: 'Maou Gakuin no Futekigousha: Shijou Saikyou no Maou no Shiso, Tensei shite Shison-tachi no Gakkou e Kayou II',
+        poster: "https://cdn.myanimelist.net/images/anime/1471/128323.jpg",
+        link: 'maougakuin2.html'
+    },
+    {
+        id: '23',
+        titleName: 'Tomo-chan wa Onnanoko!',
+        poster: "https://cdn.myanimelist.net/images/anime/1444/131828.jpg",
+        link: 'tomochan.html'
+    },
+    {
+        id: '24',
+        titleName: 'Otonari no Tenshi-sama ni Itsunomanika Dame Ningen ni Sareteita Ken',
+        poster: "https://cdn.myanimelist.net/images/anime/1209/132816.jpg",
+        link: 'otonari.html'
+    },    
+    {
+        id: '25',
+        titleName: 'Koori Zokusei Danshi to Cool na Douryou Joshi',
+        poster: "https://cdn.myanimelist.net/images/anime/1927/132758.jpg",
+        link: 'koori.html'
+    },    
+    {
+        id: '26',
+        titleName: 'Isekai Nonbiri Nouka',
+        poster: "https://cdn.myanimelist.net/images/anime/1983/132329.jpg",
+        link: 'nobiri.html'
+    },    
+    {
+        id: '27',
+        titleName: 'Spy Kyoushitsu',
+        poster: "https://cdn.myanimelist.net/images/anime/1491/132864.jpg",
+        link: 'spykyo.html'
+    },    
+    {
+        id: '28',
+        titleName: 'Inu ni Nattara Suki na Hito ni Hirowareta (Versi Haram Bosquu)',
+        poster: "https://cdn.myanimelist.net/images/anime/1832/132333.jpg",
+        link: 'inu.html'
+    },
+    {    
+        id: '29',
+        titleName: 'Saikyou Onmyouji no Isekai Tenseiki',
+        poster: "https://cdn.myanimelist.net/images/anime/1547/125900.jpg",
+        link: 'saikyou.html'
+    },
+    {    
+        id: '30',
+        titleName: 'Sugar Apple Fairy Tale',
+        poster: "https://cdn.myanimelist.net/images/anime/1897/131300.jpg",
+        link: 'sugar.html'
+    },
+    {    
+        id: '31',
+        titleName: 'Bocchi the Rock!',
+        poster: "https://cdn.myanimelist.net/images/anime/1448/127956.jpg",
+        link: 'bocchi.html'
+    },
+    {    
+        id: '32',
+        titleName: 'Sword Art Online: Alicization',
+        poster: "https://cdn.myanimelist.net/images/anime/1993/93837.jpg",
+        link: 'saoali.html'
+    },
+    {    
+        id: '33',
+        titleName: 'Gamers!',
+        poster: "https://cdn.myanimelist.net/images/anime/4/86828.jpg",
+        link: 'gamer.html'
+    },
+    {    
+        id: '34',
+        titleName: 'Darling in the FranXX',
+        poster: "https://cdn.myanimelist.net/images/anime/1614/90408.jpg",
+        link: 'dfxx.html'
+    },
+    {    
+        id: '35',
+        titleName: 'Eromanga-sensei',
+        poster: "https://cdn.myanimelist.net/images/anime/2/86468.jpg",
+        link: 'eromanga.html'
+    },
+    {    
+        id: '36',
+        titleName: 'Golden Time',
+        poster: "https://cdn.myanimelist.net/images/anime/12/52091.jpg",
+        link: 'golden.html'
+    },
+    {    
+        id: '37',
+        titleName: 'Saenai Heroine no Sodatekata',
+        poster: "https://cdn.myanimelist.net/images/anime/7/68783.jpg",
+        link: 'saekano.html'
+    },
+    {    
+        id: '38',
+        titleName: 'Takt Op. Destiny',
+        poster: "https://cdn.myanimelist.net/images/anime/1449/117797.jpg",
+        link: 'taktop.html'
+    },
+    {    
+        id: '39',
+        titleName: 'Arifureta Shokugyou de Sekai Saikyou',
+        poster: "https://cdn.myanimelist.net/images/anime/1776/97682.jpg",
+        link: 'arifureta.html'
+    },
+    {    
+        id: '40',
+        titleName: 'Arifureta Shokugyou de Sekai Saikyou 2nd Season',
+        poster: "https://cdn.myanimelist.net/images/anime/1877/119668.jpg",
+        link: 'arifuretas2.html'
+    },
+    {    
+        id: '41',
+        titleName: 'Bokutachi wa Benkyou ga Dekinai',
+        poster: "https://cdn.myanimelist.net/images/anime/1602/100510.jpg",
+        link: 'bokuben.html'
+    },
+    {    
+        id: '42',
+        titleName: 'Bokutachi wa Benkyou ga Dekinai!',
+        poster: "https://cdn.myanimelist.net/images/anime/1974/102960.jpg",
+        link: 'bokubens2.html'
+    },
+    {    
+        id: '43',
+        titleName: 'Sword Art Online',
+        poster: "https://cdn.myanimelist.net/images/anime/11/39717.jpg",
+        link: 'sao.html'
+    },
+    {    
+        id: '44',
+        titleName: 'Sword Art Online II',
+        poster: "https://cdn.myanimelist.net/images/anime/1223/121999.jpg",
+        link: 'sao2.html'
+    },
+    {    
+        id: '45',
+        titleName: 'Tanaka-kun wa Itsumo Kedaruge',
+        poster: "images/tanakakun.jpg",
+        link: 'tanaka.html'
+    },
+    {    
+        id: '46',
+        titleName: 'Tanaka-kun wa Itsumo Kedaruge',
+        poster: "images/tanakakun.jpg",
+        link: 'tanaka.html'
+    },
+    {    
+        id: '47',
+        titleName: 'Tanaka-kun wa Itsumo Kedaruge',
+        poster: "images/tanakakun.jpg",
+        link: 'tanaka.html'
+    },
+    {    
+        id: '48',
+        titleName: 'Tanaka-kun wa Itsumo Kedaruge',
+        poster: "images/tanakakun.jpg",
+        link: 'tanaka.html'
+    },
+    {    
+        id: '49',
+        titleName: 'Tanaka-kun wa Itsumo Kedaruge',
+        poster: "images/tanakakun.jpg",
+        link: 'tanaka.html'
+    },
+    {    
+        id: '50',
+        titleName: 'Tanaka-kun wa Itsumo Kedaruge',
+        poster: "images/tanakakun.jpg",
+        link: 'tanaka.html'
+    },
+    {    
+        id: '51',
+        titleName: 'Tanaka-kun wa Itsumo Kedaruge',
+        poster: "images/tanakakun.jpg",
+        link: 'tanaka.html'
+    },
+    {    
+        id: '52',
+        titleName: 'Tanaka-kun wa Itsumo Kedaruge',
+        poster: "images/tanakakun.jpg",
+        link: 'tanaka.html'
+    },
+    {    
+        id: '53',
+        titleName: 'Tanaka-kun wa Itsumo Kedaruge',
+        poster: "images/tanakakun.jpg",
+        link: 'tanaka.html'
+    },
+    {    
+        id: '54',
+        titleName: 'Tanaka-kun wa Itsumo Kedaruge',
+        poster: "images/tanakakun.jpg",
+        link: 'tanaka.html'
+    },
+    {    
+        id: '55',
+        titleName: 'Tanaka-kun wa Itsumo Kedaruge',
+        poster: "images/tanakakun.jpg",
+        link: 'tanaka.html'
+    },
+    {    
+        id: '56',
+        titleName: 'Tanaka-kun wa Itsumo Kedaruge',
+        poster: "images/tanakakun.jpg",
+        link: 'tanaka.html'
+    },
+    {    
+        id: '57',
+        titleName: 'Tanaka-kun wa Itsumo Kedaruge',
+        poster: "images/tanakakun.jpg",
+        link: 'tanaka.html'
+    },
+    {    
+        id: '58',
+        titleName: 'Tanaka-kun wa Itsumo Kedaruge',
+        poster: "images/tanakakun.jpg",
+        link: 'tanaka.html'
+    },
+
+
+]
+
+let search_result = document.getElementsByClassName('search_result')[0];
+
+
+anime.forEach(Element => {
+    const {id, titleName, poster, link} = Element;
+    let card = document.createElement('a');
+    card.classList.add('card');
+    card.href = link;
+    card.innerHTML = `
+    <img src="${poster}" alt=""> 
+    <div class="content">
+        ${titleName}
+    </div>
+    `;
+    search_result.appendChild(card);
+});
+
+let input = document.getElementsByTagName('input')[0];
+
+input.addEventListener('keyup', ()=>{
+        let input_value = input.value.toUpperCase();
+        let items = search_result.getElementsByTagName('a');
+
+        for (let i = 0; i < items.length; i++) {
+            let as = items[i].getElementsByClassName('content')[0];
+            let text_value = as.textContent || as.innerText; 
+
+            if (text_value.toUpperCase().indexOf(input_value) > -1) {
+                items[i].style.display = "flex";
+            }   
+            else {
+                items[i].style.display = "none"; 
+            }
+            if (input.value == 0) {
+                search_result.style.display = "none";
+            }
+            else {
+                search_result.style.display = "";
+            }
+        }
+})
